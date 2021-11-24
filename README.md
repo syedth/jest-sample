ff1a85eb04d2be836466eb6696135612993acdc3
Syed <syed@Syeds-MacBook-Pro.local>
2021-11-24 13:11:49 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+### Jest Sample Repo
+Sample Repo for Jest
+
+#### Command to run 
+- npm run test
---
+++ test.js
@@ -0,0 +1,7 @@
+describe('Test', () => {
+    test('adds 1 + 2 to equal 3', () => {
+        var a = 1;
+        var b = 2;
+        expect((a + b)).toBe(3);
+    });
+});
